cf3eefc9fe07b30a110e4c365e9680d18472b96d
mehidy <mehidy.gb@gmail.com>
2025-02-15 18:43:41 +0600

    feat: enhance SpinnerResource to format spin time and include today's date

--- app/Http/Resources/SpinnerResource.php
+++ app/Http/Resources/SpinnerResource.php
@@ -14,11 +14,26 @@ class SpinnerResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $todaySpinTime = now();
+        $todaySpinTime->setTime(
+            $this->spin_time->hour,
+            $this->spin_time->minute,
+            $this->spin_time->second
+        )->format('Y-m-d H:i:s');
+
         return [
             'id' => $this->id,
             'rotation_point' => $this->rotation_point,
-            'spin_time' => $this->spin_time->format('Y-m-d H:i:s'),
-            'spin_time_in_ms' => $this->spin_time->timestamp * 1000,
+
+            // Format time with AM/PM
+            'spin_time' => $this->spin_time->format('h:i:s A'),
+
+            // Set today's date but keep the time from spin_time
+            'spin_time_with_today_date' => $todaySpinTime,
+
+            // Convert time to milliseconds without date
+            'spin_time_in_ms' => $todaySpinTime->timestamp * 1000,
+
             'created_at' => $this->created_at,
         ];
     }
