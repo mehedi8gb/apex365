3e0bf421859d6b8e1de31baeba181aa8446bf7c2
mehidy <mehidy.gb@gmail.com>
2025-02-16 07:44:44 +0600

    feat: refactor UserResource to streamline commissions data handling and improve pagination structure

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -2,14 +2,11 @@
 
 namespace App\Http\Resources;
 
-use App\Models\Commission;
-use App\Models\Leaderboard;
-use App\Models\ReferralCode;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class UserResource extends JsonResource
 {
-    protected mixed $commissions;
+    protected $commissions;
 
     public function __construct($resource, $commissions = null)
     {
@@ -27,23 +24,17 @@ class UserResource extends JsonResource
             'balance' => optional($this->account)->balance,
             'nid' => $this->nid,
             'address' => $this->address,
-            'referral_code' => optional($this->referralCode->referralCode)->code, // Ensure this is correctly accessed
+            'referral_code' => optional($this->referralCode->referralCode)->code,
             'leaderboard' => new LeaderboardResource($this->whenLoaded('leaderboard')),
-            'commissions' => [
-                'data' => CommissionResource::collection($this->commissions->items()), // Only paginated data
-                'pagination' => [
-                    'total' => $this->commissions->total(),
-                    'per_page' => $this->commissions->perPage(),
-                    'current_page' => $this->commissions->currentPage(),
-                    'last_page' => $this->commissions->lastPage(),
-                    'next_page_url' => $this->commissions->nextPageUrl(),
-                    'prev_page_url' => $this->commissions->previousPageUrl(),
-                ]
-            ]
+            'commissions' => CommissionResource::collection($this->whenLoaded('commissions')),
+            'pagination' => [
+                'total' => $this->commissions->total(),
+                'per_page' => $this->commissions->perPage(),
+                'current_page' => $this->commissions->currentPage(),
+                'last_page' => $this->commissions->lastPage(),
+                'next_page_url' => $this->commissions->nextPageUrl(),
+                'prev_page_url' => $this->commissions->previousPageUrl(),
+            ],
         ];
     }
 }
-
-
-
-
