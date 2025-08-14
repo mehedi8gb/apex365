ea6bac3e2f1427c85da748594ef10af4fc15061c
mehidy <mehidy.gb@gmail.com>
2025-08-14 16:34:58 +0600

    feat: integrate dynamic profile rank configurations and commission service bootstrap logic

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Exceptions\Handler;
+use App\Services\CommissionService;
 use App\Services\ProfileRankService;
 use Illuminate\Contracts\Debug\ExceptionHandler;
 use Illuminate\Support\ServiceProvider;
@@ -18,26 +19,48 @@ class AppServiceProvider extends ServiceProvider
         $this->app->singleton(ProfileRankService::class, function () {
             return new ProfileRankService([
                 (object) [
+                    'name' => 'Pro Platinum',
+                    'threshold' => 10,
+                    'coins' => 30000.0,
+                ],
+                (object) [
+                    'name' => 'Platinum',
+                    'threshold' => 8,
+                    'coins' => 20000.0,
+                ],
+                (object) [
                     'name' => 'Diamond',
-                    'threshold' => 50,
-                    'coins' => 1000.0
+                    'threshold' => 6,
+                    'coins' => 10000.0,
                 ],
                 (object) [
                     'name' => 'Gold',
-                    'threshold' => 23,
-                    'coins' => 50.0
+                    'threshold' => 4,
+                    'coins' => 5000.0,
                 ],
-                (object) ['name' => 'Silver', 'threshold' => 10, 'coins' => 30.0],
-                (object) ['name' => 'Bronze', 'threshold' => 5, 'coins' => 5.0],
+                (object) [
+                    'name' => 'Silver',
+                    'threshold' => 2,
+                    'coins' => 2500.0,
+                ],
+                (object) [
+                    'name' => 'Bronze',
+                    'threshold' => 1,
+                    'coins' => 1000.0,
+                ],
             ]);
         });
+
     }
 
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(CommissionService $service): void
     {
-        //
+        $commissions = $service->getAll();
+
+        // Dynamically override config
+        config(['commissions' => $commissions]);
     }
 }
