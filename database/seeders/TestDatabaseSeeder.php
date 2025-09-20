d4ba2d6beebcb850ba559a7f30819c9683233ea7
Mehidy <mehidy.gb@gmail.com>
2025-09-20 10:41:01 +0600

    fresh testing env

---
+++ database/seeders/TestDatabaseSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class TestDatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        $this->call([
+            RolesAndPermissionsSeeder::class,
+            CommissionSettingsSeeder::class,
+            AdminRankSettingSeeder::class,
+            TransactionSeeder::class,
+            ReferralCodeSeeder::class,
+            UsersSeeder::class,
+        ]);
+    }
+}
